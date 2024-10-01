232b0b5a95c0948bd06eab252c382103c2b4a539
Jakub Huspenina <jakub.huspenina@gmail.com>
2024-10-01 15:27:33 +0200

    cmake projekt

---
+++ cv3/Huspeninaprojekt/Huspeninaprojekt.cpp
@@ -0,0 +1,26 @@
+﻿// Huspeninaprojekt.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "Huspeninaprojekt.h"
+
+using namespace std;
+
+int main() {
+    
+    float time, distance;
+    const float speed_of_sound = 340.0;  
+
+    
+    cout << "Zadejte cas v sekundach, za ktery se ozval hrom po blesku: ";
+    if (!(cin >> time)) {
+        cout << "Neplatny vstup! Prosim, zadejte cislo." << endl;
+        return 1;
+    }
+
+   
+    distance = time * speed_of_sound;
+
+    cout << "Blesk uhodil cca " << distance << " metru od vas." << endl;
+
+    return 0;
+}
