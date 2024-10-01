72548f53dca0c8e3531d403a8e7bf1aaf7cb53de
Jakub Huspenina <jakub.huspenina@gmail.com>
2024-10-01 15:30:44 +0200

    projekt

---
+++ cv3/FunExt/FunExt.cpp
@@ -0,0 +1,24 @@
+﻿// FunExt.cpp : Defines the entry point for the application.
+//
+
+#include "FunExt.h"
+
+using namespace std;
+
+int main()
+{
+    double minY = DBL_MAX;
+    int minX = INT_MAX;
+    for (int x = 1; x <= 10; ++x)
+    {
+        double y = 5 - 3 * x + 2 * (x - 5) * (x - 5) - (x - 10) * (x - 10) * (x - 10);
+
+        if (y < minY)
+        {
+            minY = y;
+            minX = x;
+        }
+    }
+    printf("Minimum value: %lf at x=%d\n", minY, minX);
+    return 0;
+}
